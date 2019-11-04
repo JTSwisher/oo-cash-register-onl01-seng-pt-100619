a76aaa7696aeb08e204847f5bab9af8aa0aa9d04
Jeff Swisher <jefftswisher@gmail.com>
2019-11-04 18:00:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+  
+  attr_accessor :total, :discount, :items, :last_transaction 
+  
+def initialize(discount = 0 )
+  @total = 0
+  @discount = discount 
+  @items = []
+end 
 
+  def add_item(item, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times do 
+    items << item 
+   end 
+  end 
+
+  def apply_discount
+    if discount != 0
+      
+      
+    else 
+      "There is no discount to appply."
+    end 
+  end 
+  
+  def last_transaction
+    
+  end 
+  
+end 
+
+ 
